931bb8b687e1bbcc4389d61152b48160eeef476b
zsk612 <zhangsk612@gmail.com>
2020-12-08 18:08:58 +0800

    Use StringBuilder in ZigZag Conversion to improve performance

--- Solutions/Problems/Medium/ZigZagConversion.java
+++ Solutions/Problems/Medium/ZigZagConversion.java
@@ -1,15 +1,15 @@
 public class ZigZagConversion {
-    public static String convert(String s, int numRows) {
+    public String convert(String s, int numRows) {
         if (numRows == 1) {
             return s;
         }
-        String result = new String();
+        StringBuilder result = new StringBuilder();
         int limit = numRows - 1;
         int index = 0;
         for (int i = 0; i < Math.min(numRows, s.length()); i++) {
             index = i;
             int previous = index;
-            result += s.charAt(index);
+            result.append(s.charAt(index));
             Boolean isEven = true;
             while (index <= s.length()) {
                 previous = index;
@@ -17,17 +17,17 @@ public class ZigZagConversion {
                     index += 2 * (limit - i);
                     isEven = false;
                 } else {
-                    index += 2 * (limit - (limit - i));
+                    index += 2 * i;
                     isEven = true;
                 }
                 if ((index != previous) && (index < s.length())) {
-                    result += s.charAt(index); 
+                    result.append(s.charAt(index));
                 }
             }
         }
-        return result; 
+        return result.toString(); 
     }
-
+    
     public static void main(String[] args) {
         String s = new String("PAYPALISHIRING");
         int numRows = 1;
