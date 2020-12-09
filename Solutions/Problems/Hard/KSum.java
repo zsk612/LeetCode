07f44f983b086b7bdf26793bbd62e18b24e9ab6c
zsk612 <zhangsk612@gmail.com>
2020-12-09 18:14:20 +0800

    Add online solution to KSum

---
+++ Solutions/Problems/Hard/KSum.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class KSum {
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        return kSum(nums, target, 0, 4);
+    }
+    public static List<List<Integer>> kSum(int[] nums, int target, int start, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (start == nums.length || nums[start] * k > target || target > nums[nums.length - 1] * k)
+            return res;
+        if (k == 2)
+            return twoSum(nums, target, start);
+        for (int i = start; i < nums.length; ++i)
+            if (i == start || nums[i - 1] != nums[i])
+                for (var set : kSum(nums, target - nums[i], i + 1, k - 1)) {
+                    res.add(new ArrayList<>(Arrays.asList(nums[i])));
+                    res.get(res.size() - 1).addAll(set);
+                }
+        return res;
+    }
+    public static List<List<Integer>> twoSum(int[] nums, int target, int start) {
+        List<List<Integer>> res = new ArrayList<>();
+        Set<Integer> s = new HashSet<>();
+        for (int i = start; i < nums.length; ++i) {
+            if (res.isEmpty() || res.get(res.size() - 1).get(1) != nums[i])
+                if (s.contains(target - nums[i]))
+                    res.add(Arrays.asList(target - nums[i], nums[i]));
+            s.add(nums[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int [] numArray = {2,0,-1,-3,2,1,3,0,-4};
+        int target = 0;
+        System.out.println(fourSum(numArray, target));
+    }
+
+}
