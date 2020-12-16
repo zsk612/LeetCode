37e4278121f08b1981341d52e9a31a4ee16ccebe
zsk612 <zhangsk612@gmail.com>
2020-12-16 18:15:24 +0800

    Add online solution to min delete distance

---
+++ Solutions/Problems/Medium/MinDeleteDistance.java
@@ -0,0 +1,39 @@
+public class MinDeleteDistance {
+    public static int minDistance1(String word1, String word2) {
+        int dp[][] = new int[word1.length()+1][word2.length()+1];
+        for(int i = 0; i <= word1.length(); i++) {
+            for(int j = 0; j <= word2.length(); j++) {
+                if(i == 0 || j == 0) dp[i][j] = 0;
+                else dp[i][j] = (word1.charAt(i-1) == word2.charAt(j-1)) ? dp[i-1][j-1] + 1
+                        : Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        int val =  dp[word1.length()][word2.length()];
+        return word1.length() - val + word2.length() - val;
+    }
+
+    public static int minDistance2(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = i + j;
+                else if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+
+    public static void main(String[] args) {
+        String s1 = new String("holtfwfwgrwt");
+        String s2 = new String("bottlqfqqrwee");
+        int result1 = minDistance1(s1, s2);
+        int result2 = minDistance2(s1, s2);
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
